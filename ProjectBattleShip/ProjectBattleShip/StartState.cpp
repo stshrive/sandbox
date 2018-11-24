4377e5c5087bb0b1903fa60d9e3ae02b09ad7188
stshrive <>
2018-11-23 17:29:31 -0800

    Use random generator

--- ProjectBattleShip/ProjectBattleShip/StartState.cpp
+++ ProjectBattleShip/ProjectBattleShip/StartState.cpp
@@ -3,6 +3,32 @@
 
 #include <random>
 #include <vector>
+#include <chrono>
+#include <algorithm>
+#include <array>
+#include <functional>
+
+static std::vector<OpponentAction> actions = {
+      OpponentAction::MoveDown
+    , OpponentAction::MoveLeft
+    , OpponentAction::MoveRight
+    , OpponentAction::MoveUp
+    , OpponentAction::RotateShip
+    , OpponentAction::PlaceShip
+};
+
+using std::chrono::milliseconds;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+
+std::array<int, 653> seed_;
+std::random_device rdev;
+bool seeded = false;
+static std::mt19937 generator;
+std::uniform_int_distribution<int> distribution(1, actions.size());
+
+OpponentAction ChooseAction();
+
 
 void Start::Enter(BattleShipOpponent* entity)
 {
@@ -11,16 +37,27 @@ void Start::Enter(BattleShipOpponent* entity)
 
 void Start::Execute(BattleShipOpponent* entity)
 {
-    std::vector<OpponentAction> actions;
-    for (int i = 2; i <= 64; i *= 2)
+    OpponentAction action = ChooseAction();
+    if (action == OpponentAction::PlaceShip)
     {
-        actions.push_back(static_cast<OpponentAction>(i));
+        entity->placed_ships++;
     }
-
-    std::random_shuffle(actions.begin(), actions.end());
-    entity->AddAction(actions.front(), Coordinates());
+    entity->AddAction(action, Coordinates());
 }
 
 void Start::Exit(BattleShipOpponent* entity)
 {
+}
+
+OpponentAction ChooseAction()
+{
+    if (!seeded)
+    {
+        std::generate_n(seed_.data(), seed_.size(), std::ref(rdev));
+        std::seed_seq seed(std::begin(seed_), std::end(seed_));
+        generator.seed(seed);
+        seeded = true;
+    }
+    int choice = distribution(generator);
+    return actions[choice - 1];
 }
