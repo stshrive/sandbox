de954a9e30d707f513949e35b9ecb47c10c66636
stshrive <>
2018-11-23 17:24:00 -0800

    reorder functions

--- ProjectBattleShip/ProjectBattleShip/Game.h
+++ ProjectBattleShip/ProjectBattleShip/Game.h
@@ -8,15 +8,17 @@ class Ship;
 enum Movement;
 typedef struct ButtonSpecs mButton;
 
+bool GameInitialize();
+bool Setup(std::map<int, std::pair<Ship*, bool>> &ships);
+bool MoveShip(Ship * ship, int map[][11], Movement movement);
+
 void NewGame();
 void EnemyTurn();
-bool Setup(std::map<int, std::pair<Ship*, bool>> &ships);
-void MoveShip(Ship * ship, int map[][11], Movement movement);
 void MoveCursor(Coordinates cursor, int map[][11], Movement movement);
 void DrawTile(BitMapObj &BmoDestination, BitMapObj &BmoSource, int x, int y, int TILE);
 void RenderMap();
 void mCreateButton_xy(mButton &button, int x, int y);
+
 POINT FindBitMapPos(int TILE);
-bool GameInitialize();
 
 #endif
