1c3c3a6d18acb08a5811a4ccf499f6899793ad18
stshrive <>
2018-11-26 18:57:48 -0800

    Remove implementation from header

--- ProjectBattleShip/ProjectBattleShip/OpponentStates.cpp
+++ ProjectBattleShip/ProjectBattleShip/OpponentStates.cpp
@@ -1,11 +1,5 @@
 #include "OpponentStates.h"
 #include "HitProbability.h"
 
-#include "MapComponents.h"
 
-std::shared_ptr<IProbability<int, Coordinates>> probability(
-    new HitProbability(BOARDSIZE));
 
-StartState  OpponentStartState;
-SearchState OpponentSearchState(probability);
-AttackState OpponentAttackState(probability);
